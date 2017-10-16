d1cf528f765528fbc211475d3fd9717f9d24d4f3
ioanniswd <ioanniswd@gmail.com>
2017-10-16 12:56:38 +0300

    Used updated s3-db with continuation token. Used getHead.
     On branch master
    
     Changes to be committed:
    	modified:   find.js
    	modified:   package-lock.json
    	modified:   package.json

--- find.js
+++ find.js
@@ -1,5 +1,8 @@
 "use strict";
 
+process.env.AWS_DEFAULT_REGION = 'eu-west-1';
+process.env.STAGE = 'dev';
+
 const DB = require('s3-db');
 const db = new DB();
 
@@ -8,11 +11,31 @@ var col;
 db.getCollection('delmanorderss3db')
   .then(collection => {
     col = collection;
-    return col.find('some');
+    var limit = 10;
+    return col.find('crepemania', 10);
   })
   .then(items => {
+
     console.log('items:', items);
-    return items[0].getDocument();
+    console.log('items.hasMore:', items.hasMore);
+    console.log('items.next:', items.next);
+    console.log('continuationToken:', items.continuationToken);
+
+    var promises = items.map(item => {
+      return col.getHead(item.id);
+    });
+
+    console.log('num of items:', items.length);
+
+    Promise.all(promises)
+      .then(results => {
+        // console.log('results:', results);
+        return items[0].getDocument();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    // console.log('items:', items);
   })
   .then(doc => console.log('doc:', doc))
   .catch(err => console.log(err));
